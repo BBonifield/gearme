35c1cf0293a5276303242cda011901818112972c
Alex McPherson <alex@quickleft.com>
2013-05-14 20:09:13 -0600

    no bb

--- js/main.js
+++ js/main.js
@@ -2,25 +2,12 @@ $(document).ready(function(){
   App.init();
 })
 
-
 App = {};
 
-App.router = Backbone.Router.extend({
-  initialize: function() {
-    
-  },
-
-  routes: {
-    '/' : 'home'
-  },
-
-  home: function() {
-    console.log('im home');
-  }
-
-});
-
 App.init = function() {
-  Backbone.history.start({ silent: true });
-  Backbone.history.loadUrl(location.pathname);
+  App.bindEvents();
+}
+
+App.bindEvents = function() {
+  
 }
